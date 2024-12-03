35346be1d2b2be435a9eecf60e170e544f2cf67d
Victor Fernandes <victor.fernandes2004@gmail.com>
2024-12-03 09:58:38 -0400

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,13 +10,12 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1,"Books");
-		System.out.println(obj);
-		
-		Seller obj1 = new Seller(1, "Victor", "victorfernandes@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(obj1);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 		
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaJDBC();
+		return new SellerDaJDBC(DB.getConnection());
 	}
 	
 }

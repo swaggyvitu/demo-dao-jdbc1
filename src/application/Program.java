364f4f5565c29e7c18b7386ed6792fe436e196ba
Victor Fernandes <victor.fernandes2004@gmail.com>
2024-12-03 16:02:47 -0400

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST3: seller findAll ===");
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }

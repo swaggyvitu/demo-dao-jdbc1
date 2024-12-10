7f4a23ec0c7a1dd678abc5615fe661c45500e6c7
Victor Fernandes <victor.fernandes2004@gmail.com>
2024-12-09 21:24:42 -0400

    Creating insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,6 +33,11 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }

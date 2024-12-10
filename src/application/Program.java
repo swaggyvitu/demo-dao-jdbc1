613bb2c1321c2e422e8d69e1aada5375cce67ab7
Victor Fernandes <victor.fernandes2004@gmail.com>
2024-12-09 21:55:44 -0400

    Creating update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,7 +37,14 @@ public class Program {
 		System.out.println("\n=== TEST4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
+		
 	}
 
 }

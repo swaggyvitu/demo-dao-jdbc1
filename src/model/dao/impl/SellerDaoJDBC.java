cdc4f97348e2b8250b9a0d9db78d4ceca8af4b23
Victor Fernandes <victor.fernandes2004@gmail.com>
2024-12-12 15:24:48 -0400

    Creating delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -97,8 +97,24 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+		finally {
+			try {
+		        if (st != null) st.close();
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+		}
 	}
 
 	@Override
